7f7bbcc4eb2ec6ae55895c7c57cdc1da0a669deb
Colby Chance <colbychance@lawn-143-215-122-9.lawn.gatech.edu>
2017-02-18 23:00:54 -0500

    first

---
+++ JS-bar.js
@@ -0,0 +1,12 @@
+var currentText = "<topTitle><b>~/ColbyChance/AboutMe</b></topTitle>";
+
+$(document).ready(function() {
+    $(window).bind('scroll', function() {
+      if ($(window).scrollTop() > ($('#jumbo').outerHeight()) - $('#coverButton').height()) {
+            document.getElementById("coverButton").innerHTML = currentText;
+       } else {
+           document.getElementById("coverButton").innerHTML = '<span class="glyphicon glyphicon-chevron-down" id="downArrow"></span>';
+        console.log("Abs" + $('#coverButton').hasClass("absBar"));
+       }
+    });
+});
---
+++ JS-main.js
@@ -0,0 +1,20 @@
+var fader = document.getElementById("underLine");
+setInterval(function() {
+    if (fader.style.visibility == "hidden") {
+        fader.style.visibility = "visible";
+    } else {
+        fader.style.visibility = "hidden";
+    }
+    }, 500)
+var cb = document.getElementById("coverButton");
+$("#myCarousel").on('slid.bs.carousel', function () {
+        if ($('div.active').index() == 1) {
+            currentText = "<topTitle><b>~/ColbyChance/AboutMe</b></topTitle>";
+        } else if ($('div.active').index() == 2) {
+            currentText = "<topTitle><b>~/ColbyChance/Projects</b></topTitle>";
+        } else if ($('div.active').index() == 0) {
+            currentText = "<topTitle><b>~/ColbyChance/Skills</b></topTitle>";
+        }
+        cb.innerHTML = currentText;
+    console.log($('div.active').index() + 1);
+    });
