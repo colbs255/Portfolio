3cc64b2cbc58765d6779a5a9cdbd9677af6cb0b8
Colby Chance <colbychance@lawn-143-215-120-156.lawn.gatech.edu>
2017-02-24 22:45:58 -0500

    title text changes earlier

--- JS-bar.js
+++ JS-bar.js
@@ -2,10 +2,12 @@ var currentText = "<topTitle><b>~/ColbyChance/AboutMe</b></topTitle>";
 
 $(document).ready(function() {
     $(window).bind('scroll', function() {
-      if ($(window).scrollTop() > ($('#jumbo').outerHeight()) - $('#coverButton').height()) {
+      if ($(window).scrollTop() > ($('#jumbo').outerHeight() / 2)) {
             document.getElementById("coverButton").innerHTML = currentText;
        } else {
            document.getElementById("coverButton").innerHTML = '<span class="glyphicon glyphicon-chevron-down navbar-text" id="downArrow"></span>';
        }
     });
 });
+
+//$(window).scrollTop() > ($('#jumbo').outerHeight()) - $('#coverButton').height()
