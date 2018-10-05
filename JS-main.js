f7667c6348a56ae98860732e7875c0316b507630
colbychance <duncanchance11@gmail.com>
2018-10-05 16:37:39 -0400

    updates

--- JS-main.js
+++ JS-main.js
@@ -8,15 +8,17 @@ setInterval(function() {
     }, 500)
 var cb = document.getElementById("coverButton");
 $("#myCarousel").on('slid.bs.carousel', function () {
+        $('html, body').animate({
+            scrollTop: $('#jumbo').outerHeight() - $('#coverButton').outerHeight()
+        }, "slow");
         if ($('div.active').index() == 1) {
             currentText = "<topTitle><b>/ColbyChance/AboutMe</b></topTitle>";
         } else if ($('div.active').index() == 2) {
             currentText = "<topTitle><b>/ColbyChance/Projects</b></topTitle>";
         } else if ($('div.active').index() == 0) {
-            currentText = "<topTitle><b>/ColbyChance/Skills</b></topTitle>";
+            currentText = "<topTitle><b>/ColbyChance/WhyMe</b></topTitle>";
         }
         cb.innerHTML = currentText;
-    console.log($('div.active').index() + 1);
     });
 
 $("#coverButton").on('click', function(e) {
