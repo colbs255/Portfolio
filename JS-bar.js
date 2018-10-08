96663ee7d4e5495478ee1b62572ec251476a7bf2
colbychance <duncanchance11@gmail.com>
2018-10-08 17:19:19 -0400

    update buttons and navigation

--- JS-bar.js
+++ JS-bar.js
@@ -1,4 +1,6 @@
 var currentText = "<topTitle><b>/ColbyChance/AboutMe</b></topTitle>";
+var leftButton = document.getElementById("myLeftButton");
+var rightButton = document.getElementById("myRightButton");
 
 $(document).ready(function() {
     $(window).bind('scroll', function() {
@@ -7,6 +9,14 @@ $(document).ready(function() {
        } else {
            document.getElementById("coverButton").innerHTML = '<span class="glyphicon glyphicon-chevron-down navbar-text" id="downArrow"></span>';
        }
+        
+//        if ($(window).scrollTop() > ($('#jumbo').outerHeight() * 0.6)) {
+//            leftButton.style.visibility = "visible";
+//            rightButton.style.visibility = "visible";
+//       } else {
+//           leftButton.style.visibility = "hidden";
+//           rightButton.style.visibility = "hidden";
+//       }
     });
 });
 
--- JS-main.js
+++ JS-main.js
@@ -7,18 +7,33 @@ setInterval(function() {
     }
     }, 500)
 var cb = document.getElementById("coverButton");
+var leftButton = document.getElementById("myLeftButton");
+var rightButton = document.getElementById("myRightButton");
 $("#myCarousel").on('slid.bs.carousel', function () {
         $('html, body').animate({
             scrollTop: $('#jumbo').outerHeight() - $('#coverButton').outerHeight()
         }, "slow");
+        leftText = ""
+        rightText = ""
+        expText = "Why Me"
+        aboutText = "About Me"
+        projectsText = "Projects"
         if ($('div.active').index() == 1) {
             currentText = "<topTitle><b>/ColbyChance/AboutMe</b></topTitle>";
+            leftText = expText
+            rightText = projectsText
         } else if ($('div.active').index() == 2) {
             currentText = "<topTitle><b>/ColbyChance/Projects</b></topTitle>";
+            leftText = aboutText;
+            rightText = expText;
         } else if ($('div.active').index() == 0) {
             currentText = "<topTitle><b>/ColbyChance/WhyMe</b></topTitle>";
+            leftText = projectsText;
+            rightText = aboutText;
         }
         cb.innerHTML = currentText;
+        leftButton.innerHTML = leftText;
+        rightButton.innerHTML = rightText;
     });
 
 $("#coverButton").on('click', function(e) {
@@ -27,4 +42,15 @@ $("#coverButton").on('click', function(e) {
         scrollTop: $('#jumbo').outerHeight() - $('#coverButton').outerHeight()
     }, "slow");
 });
+
+$("#myLeftButton").on('click', function(e) {
+    $('.carousel').carousel('prev');
+    $(this).blur();
+});
+
+$("#myRightButton").on('click', function(e) {
+    $('.carousel').carousel('next');
+    $(this).blur();
+});
+
 
